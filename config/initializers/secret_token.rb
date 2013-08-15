edba95f8e57f96c04820a1e60cd8c7e80bd58090
zmontesd <zmontesd@gmail.com>
2013-08-15 15:24:01 -0700

    initial commit with initial devise set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SplitAmongUs::Application.config.secret_token = '78a3740b769ec6d323cfb921afb7be6013ae56c656c7085af99287fc2dbc45459ab4a6ff9b3f3ae5015aed8ab4a5dbba9a8e1257332869ad311a24f50da447c6'
