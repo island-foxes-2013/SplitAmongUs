28e589cc38a802f7ab62a7f6912540a35e43ccb9
Apprentice <apprentice@dbc21.local>
2013-08-15 21:51:11 -0700

    set up test environment

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'pg'
 gem 'devise'
 
@@ -13,30 +10,26 @@ gem 'devise'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
 
-group :development do
+group :development, :test do
   gem 'sextant'
+  gem 'rspec-rails'
+  gem 'faker'
+  gem 'factory_girl_rails'
+  gem 'shoulda-matchers'
+  gem 'simplecov'
+  gem 'capybara'
+  gem 'guard-rspec' # auto-run tests
+  gem 'launchy'
+  gem 'capybara-webkit','~>1.0.0'
+  gem 'rb-fsevent' #notifies when files change
 end
 
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
+gem 'pry-rails', :group => :development
 
 # To use debugger
-# gem 'debugger'
+gem 'debugger'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,66 @@
+# simple cov needs to run before anything else
+require 'simplecov'
+SimpleCov.start 'rails'
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require "capybara/rspec"
+require 'faker'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  # include devise test helpers i.e.: 
+  # sign_in :user, @user   # sign_in(scope, resource)
+  # sign_in @user          # sign_in(resource)
+  
+  # sign_out :user         # sign_out(scope)
+  # sign_out @user         # sign_out(resource)
+  # doesn't work for integration tests driven by Capybara
+  # meant to be used with functional tests only
+  # Instead, fill in the form or explicitly set the user in session
+  # go to the url below to figure out how to test using capybara
+  # https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
+  config.include Devise::TestHelpers, :type => :controller
+
+  config.include FactoryGirl::Syntax::Methods
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+
+  config.after do
+    if example.metadata[:type] == :feature and example.exception.present?
+      save_and_open_page
+    end
+  end
+end
+
+Capybara.javascript_driver = :webkit
