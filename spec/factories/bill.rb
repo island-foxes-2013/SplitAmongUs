16aa88ff4ab92a319458dae4db36064502067bd1
zmontesd <zmontesd@gmail.com>
2013-08-17 14:22:28 -0700

    seed data base, add bill testing data, add comment to bill

--- spec/factories/bill.rb
+++ spec/factories/bill.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
 
   factory :bill do
-    amount 10.50
+    amount      { rrand(2333.43) }
+    description { Faker::Lorem.sentence }
+    date        { Date.today }
     # fill in date here using faker! with blocks {}
   end
 end
