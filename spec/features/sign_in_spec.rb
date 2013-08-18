50287bf9d7f3d3c4349a04050c1f24047a7d5a6a
Maria <mbtomori@gmail.com>
2013-08-18 14:03:57 -0700

    created tests for unhappy user login

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,9 +3,30 @@ require 'spec_helper'
 describe 'SignIn' do
   let(:user) { create(:user) }
 
+  before do
+    visit root_path
+  end
+
+  context "improper sign-in" do
+    it "doesn't allow a user to sign up with the wrong password" do
+      within '#login' do
+        fill_in 'email', with: user.email
+        fill_in 'password', with: user.password + "234"
+      end
+      page.should_not have_content("Welcome, #{user.name}")
+    end
+
+    it "doesn't allow a non-user to log in" do
+      within '#login' do
+        fill_in 'email', with: user.email.gsub!(/^\w+/, 'aeiou')
+        fill_in 'password', with: user.password
+      end
+      page.should_not have_content("Welcome, #{user.name}")
+    end
+  end
+
   context "after sign-in" do
-    it "should redirect to the dashboard page" do
-      visit root_path
+    it "redirects to the dashboard page" do
       within '#login' do 
         fill_in 'email', :with => user.email
         fill_in 'password', :with => user.password
