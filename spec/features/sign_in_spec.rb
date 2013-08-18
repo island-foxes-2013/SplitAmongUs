9a5b460894592f94eafaeae2e6571c707a9710e6
Christine <cmarquardt86@gmail.com>
2013-08-17 19:44:52 -0700

    added specs to test the sign in process

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'SignIn' do
+  let(:user) { create(:user) }
+
+  context "after sign-in" do
+    it "should redirect to the dashboard page" do
+      visit root_path
+      within '#login' do 
+        fill_in 'email', :with => user.email
+        fill_in 'password', :with => user.password
+        click_button "Log In"
+      end
+      page.should have_content("Welcome, #{user.name}")
+    end
+  end
+
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ require 'faker'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
+Capybara.javascript_driver = :webkit
 RSpec.configure do |config|
   # include devise test helpers i.e.: 
   # sign_in :user, @user   # sign_in(scope, resource)
