f66f7fe27b99bf3d2753327e385beb898705c37e
Christine <cmarquardt86@gmail.com>
2013-08-18 16:28:01 -0700

    added datepicker to the create a bill form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,6 +19,15 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
     def list_params
       params.require(:list).permit(:name, :total_amount)
