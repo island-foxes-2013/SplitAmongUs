c3cff26599deb472b0b57f2e801422c764101878
Christine <cmarquardt86@gmail.com>
2013-08-18 23:27:40 -0700

    made the add a friend feature connect with adding a friend to a list

---
+++ app/controllers/users/invitations_controller.rb
@@ -0,0 +1,27 @@
+class Users::InvitationsController < Devise::InvitationsController
+  def new
+    @list = List.find(params[:list_id])
+    self.resource = resource_class.new
+    render :new
+  end
+
+  def create
+    form_params = invite_params
+    list_id = form_params.delete(:list_id)
+
+    self.resource = resource_class.invite!(form_params, current_inviter)
+
+    if resource.errors.empty?
+      resource.groups.create(:list_id => list_id)
+      set_flash_message :notice, :send_instructions, :email => self.resource.email if self.resource.invitation_sent_at
+      respond_with resource, :location => after_invite_path_for(resource)
+    else
+      respond_with_navigational(resource) { render :new }
+    end
+  end
+
+  private
+  def invite_params
+    params.require(:user).permit(:name, :email, :list_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SplitAmongUs::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :invitations => 'users/invitations' }
   resources :users, only: [] do
     resources :bills, only: [:index]
   end
@@ -9,7 +9,7 @@ SplitAmongUs::Application.routes.draw do
   resources :lists, only: [:show, :new, :create] do # will need edit update delete and destroy
     resources :bills, only: [:show, :create, :edit, :update] # will need edit update delete and destroy
   end
-
+  
   resources :lists, only: [:edit, :update, :destroy]
 
   root :to => "home#index"
