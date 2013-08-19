d975ec1e3f5586eb31eaa453a9bbe475ba06a831
Maria <mbtomori@gmail.com>
2013-08-18 21:23:56 -0700

    building framework for list_spec

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -1,2 +1,19 @@
 require 'spec_helper'
 require 'selenium-webdriver'
+
+describe 'create a new list' do
+	
+end
+
+describe 'view a list' do
+
+end
+
+describe 'edit a list' do
+
+end
+
+describe 'delete a list' do
+end
+
+
