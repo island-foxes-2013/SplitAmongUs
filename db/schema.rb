045ef7382a0f4482225d68eac8ac3c617f69a906
Christine <cmarquardt86@gmail.com>
2013-08-19 17:20:37 -0700

    added owner id to the bills table

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130818063857) do
+ActiveRecord::Schema.define(:version => 20130820001801) do
 
   create_table "bills", :force => true do |t|
     t.string   "description"
-    t.integer  "amount_cents", :default => 0
+    t.decimal  "amount_cents", :precision => 8, :scale => 2, :default => 0.0
     t.integer  "list_id"
     t.date     "date"
     t.boolean  "paid"
     t.date     "date_paid"
-    t.datetime "created_at",                  :null => false
-    t.datetime "updated_at",                  :null => false
+    t.datetime "created_at",                                                  :null => false
+    t.datetime "updated_at",                                                  :null => false
+    t.integer  "owner_id"
   end
 
   create_table "groups", :force => true do |t|
@@ -34,9 +35,9 @@ ActiveRecord::Schema.define(:version => 20130818063857) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
-    t.integer  "total_amount_cents", :default => 0
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.decimal  "total_amount_cents", :precision => 8, :scale => 2, :default => 0.0
+    t.datetime "created_at",                                                        :null => false
+    t.datetime "updated_at",                                                        :null => false
   end
 
   create_table "users", :force => true do |t|
