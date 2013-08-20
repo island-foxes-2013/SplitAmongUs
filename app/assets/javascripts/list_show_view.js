a9f1a7832b8aa80acb3bc7b079dc34be70c0b7fd
zmontesd <zmontesd@gmail.com>
2013-08-19 18:42:53 -0700

    refactor bill create and render on the list#show

--- app/assets/javascripts/list_show_view.js
+++ app/assets/javascripts/list_show_view.js
@@ -4,8 +4,14 @@ function ListShowView(locator) {
   var self = this;
   dialog.form.on('ajax:success', function(e, bill) {
     self.element.find('.bills').append(bill);
+    self.refreshStats();
   });
+  
   $( "#create-bill" ).click(function() {
     dialog.open();
   });
+}
+
+ListShowView.prototype.refreshStats = function() {
+  // call the server to re-render the list-amounts div
 }
