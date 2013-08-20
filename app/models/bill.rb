a2a7c30d968b11c03827e81f42c7d1f414c0f61c
Maria <mbtomori@gmail.com>
2013-08-20 15:54:05 -0700

    created table for all bills, made columns go to the bottom

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,8 +2,13 @@ class Bill < ActiveRecord::Base
   belongs_to :list
   belongs_to :user
 
-  monetize :amount_cents  
+  monetize :amount_cents 
+  monetize :shared_amount_cents 
   
   validates :description, :amount, :date, :list_id, :user_id, presence: true
+
+  def shared_amount_cents
+  	amount_cents / list.users.count
+  end
 end
 
