6d38c211ed3c1d5a45c940e5c2703dfc46da4042
zmontesd <zmontesd@gmail.com>
2013-08-19 18:42:42 -0700

    refactor bill create and render on the list#show

--- app/assets/javascripts/bills.js
+++ app/assets/javascripts/bills.js
@@ -1,41 +1,52 @@
-$(function() {
-    var name = $( "#name" ),
-      email = $( "#email" ),
-      password = $( "#password" ),
-      allFields = $( [] ).add( name ).add( email ).add( password ),
-      tips = $( ".validateTips" );
+function Bills() {
+  this.bills = [];
+}
+
+Bills.prototype.add = function(bill) {
+  this.bills.push(bill);
+  // I might not need the trigger here -- find out...
+  $(this).trigger('added', bill);
+}
+
+
+// $(function() {
+//     var name = $( "#name" ),
+//       email = $( "#email" ),
+//       password = $( "#password" ),
+//       allFields = $( [] ).add( name ).add( email ).add( password ),
+//       tips = $( ".validateTips" );
  
-    function updateTips( t ) {
-      tips
-        .text( t )
-        .addClass( "ui-state-highlight" );
-      setTimeout(function() {
-        tips.removeClass( "ui-state-highlight", 1500 );
-      }, 500 );
-    }
+//     function updateTips( t ) {
+//       tips
+//         .text( t )
+//         .addClass( "ui-state-highlight" );
+//       setTimeout(function() {
+//         tips.removeClass( "ui-state-highlight", 1500 );
+//       }, 500 );
+//     }
  
-    $( "#dialog-bills-form" ).dialog({
-      autoOpen: false,
-      height: 300,
-      width: 350,
-      modal: true,
-      buttons: {
-        "Create bill": function() {
-          $('form#new_bill').submit();
-          $( this ).dialog( "close" );
-          // handle errors in there or make a js function to handle all errors
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      },
-      close: function() {
-        allFields.val( "" );
-      }
-    });
+//     $( "#dialog-bills-form" ).dialog({
+//       autoOpen: false,
+//       height: 300,
+//       width: 350,
+//       modal: true,
+//       buttons: {
+//         "Create bill": function() {
+//           $('form#new_bill').submit();
+//           $( this ).dialog( "close" );
+//           // handle errors in there or make a js function to handle all errors
+//         },
+//         Cancel: function() {
+//           $( this ).dialog( "close" );
+//         }
+//       },
+//       close: function() {
+//         allFields.val( "" );
+//       }
+//     });
  
-    $( ".create-bill" )
-      .click(function() {
-        $( "#dialog-bills-form" ).dialog( "open" );
-      });
-  });
+//     $( ".create-bill" )
+//       .click(function() {
+//         $( "#dialog-bills-form" ).dialog( "open" );
+//       });
+//   });
