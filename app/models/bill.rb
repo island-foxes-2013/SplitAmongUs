0e750092579e61f28dbe001fdf2c59e1e6c96fb9
Maria <mbtomori@gmail.com>
2013-08-20 19:54:51 -0700

    rendered bills in single table

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -15,5 +15,18 @@ class Bill < ActiveRecord::Base
   def amount_in_cents
     amount.fractional
   end
+
+  def paid_by
+    list.users.find do |user|
+      user.id == user_id
+    end
+  end
+  # def 
+  #   if user == current_user
+  #     You lent: (bill.amount / List.find(bill.list_id).users.count) * (List.find(bill.list_id).users.count - 1)
+  #   else 
+  #     User.find(bill.user_id).name  lent you: bill.amount / List.find(bill.list_id).users.count 
+  #   end
+  # end
 end
 
