f6e8f2df292cfeb31a75948466755447533a4720
Christine <cmarquardt86@gmail.com>
2013-08-22 12:20:19 -0700

    fixed bill model spec test

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -1,58 +1,45 @@
 require 'spec_helper'
-require 'selenium-webdriver'
 
 describe 'managing lists' do
 
-  let(:user) { create(:user) }
-  let(:list) { create(:list) }
-  let!(:bill_1) { list.bills.create(amount: 950.00, description: "Rent", date: Date.today, user: user) }
-  let!(:bill_2) { list.bills.create(amount: 75.00, description: "Electricity", date: Date.today, user: user) }
+  let(:user) { create(:user, :with_list) }
+  let!(:bill_1) { user.lists.last.bills.create(amount: 950.00, description: "Rent", date: Date.today, user: user) }
+  let!(:bill_2) { user.lists.last.bills.create(amount: 75.00, description: "Electricity", date: Date.today, user: user) }
 
+  before do 
+    login_as(user)
+  end
 
   context 'create a new list' do 
 
-    before do
-      
+    it "allows a user to create a list within a modal", js: true do
       visit root_path
-      within '#login' do 
-        fill_in 'email', with: user.email
-        fill_in 'password', with: user.password
-        page.execute_script "$('form#new_session').trigger('submit');"
+      within ".create" do 
+        click_on "add"
+        fill_in("List Name",  with: "New List")
+        click_on "Create list"
       end
-      visit root_path
+      within ".lists" do
+        page.should have_content("New List")
+      end
     end
-
-    it "a modal appears when the create list button is clicked", js: true do
-      click_link 'add'
-      fill_in 'List Name',  with: list.name
-    end
-
-    it "shows the new list on the dashboard", js: true do
-      click_link 'add'
-      fill_in 'List Name',  with: list.name
-      page.execute_script "$('form#new_session').trigger('submit');"
-      page.should have_content(list.name)
-    end
-
   end
 
   context 'does not create a list' do
 
-    before do
-      visit root_path
-      within '#login' do 
-        fill_in 'email', with: user.email
-        fill_in 'password', with: user.password
-        page.execute_script "$('form#new_session').trigger('submit');"
-        end
-      visit dashboard_index_path
+    before do 
+      login_as(user)
     end
     
     it "does not save a list without a name", js: true do
-      click_link 'add'
-      fill_in 'List Name',  with: ''
-      page.execute_script "$('form#new_session').trigger('submit');"
-      page.should have_content("Name can't be blank")
+      save_and_open_page
+      visit root_path
+      within '.create' do 
+        click_on 'add'
+        fill_in("List Name",  with: nil)
+        click_on "Create list"
+      end
+      page.should have_content("NO SOUP FOR YOU")
     end
 
     # it "does not save a list when cancel is pressed" do
@@ -69,25 +56,22 @@ describe 'managing lists' do
   context 'view a list' do 
 
     before do
+      login_as(user)
       visit root_path
-      within '#login' do 
-        fill_in 'email', with: user.email
-        fill_in 'password', with: user.password
-        click_button 'Log In'
+      within '.create' do 
+        click_on 'add'
+        fill_in("List Name",  with: nil)
+        click_on "Create list"
       end
-      visit dashboard_index_path
-      click_link 'add'
-      fill_in 'List Name',  with: list.name
-      page.execute_script "$('form#new_session').trigger('submit');"
     end
     
     it "displays the title of the list on a click" do
-      click_link(list.name)
+      click_on "#{list.name}"
       page.should have_content(list.name)
     end
 
     it "displays the total of all bills in the list" do
-      click_link(list.name)
+      click_on "#{list.name}"
       page.should have_content(1025.00)
     end
 
@@ -119,15 +103,9 @@ describe 'managing lists' do
 
   context 'edit a list' do
     
-    before do
-      visit root_path
-      within '#login' do 
-        fill_in 'email', with: user.email
-        fill_in 'password', with: user.password
-        page.execute_script "$('form#new_session').trigger('submit');"
-        end
-      visit dashboard_index_path
-    end    
+    before do 
+      login_as(user)
+    end   
     
     it "allows the user to change the name of the list" do
       pending
@@ -162,15 +140,9 @@ describe 'managing lists' do
 
   context 'delete a list' do
 
-    before do
-      visit root_path
-      within '#login' do 
-        fill_in 'email', with: user.email
-        fill_in 'password', with: user.password
-        page.execute_script "$('form#new_session').trigger('submit');"
-        end
-      visit dashboard_index_path
-    end    
+    before do 
+      login_as(user)
+    end  
   
     it "allows a user to delete a list" do
       pending
