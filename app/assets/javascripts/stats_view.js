fb55feccf5df0142c007b9bd7c266306228dfe62
zmontesd <zmontesd@gmail.com>
2013-08-21 23:32:09 -0700

    debugging statview js

--- app/assets/javascripts/stats_view.js
+++ app/assets/javascripts/stats_view.js
@@ -1,7 +1,11 @@
 function StatsView(lists) {
   this.element = $('#dashboard-stats');
   var self = this;
+  console.log('this is element'+ this.element)
+  console.log('this is self'+ self)
   $(lists).on('added', function(e, list) {
+      console.log('this is e'+ e)
+      console.log('this is list'+ list)
     $(list).on('changed', function() {
       self.refresh();
     });
@@ -11,6 +15,7 @@ function StatsView(lists) {
 StatsView.prototype.refresh  = function() {
   var self = this;
   $.get('/stats').done(function(response){
+    console.log('this is response'+ response)
     self.element.html(response);
   });
 }
