8de26d24b058a042c58e751ecab71161b84c2f15
Christine <cmarquardt86@gmail.com>
2013-08-27 23:45:38 -0700

    fixing email functionality to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,8 @@ SplitAmongUs::Application.configure do
   config.assets.digest = true
 
   config.action_mailer.delivery_method = :smtp
+  config.action_controller.default_url_options = {:host => "www.splitamong.us"}
+
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
